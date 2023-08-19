9fd4de1a5a95225887ce2f9168da34abc4e3732d
tejasvieducates <142652931+tejasvieducates@users.noreply.github.com>
2023-08-19 12:11:07 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,41 @@
+- <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tejasvi Educates</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #000000;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+    }
+    .container {
+      text-align: center;
+      padding: 20px;
+      background-color: #0074D9;
+      border-radius: 10px;
+      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+    }
+    h1 {
+      color: #FFFFFF;
+      margin-bottom: 10px;
+    }
+    p {
+      color: #DDDDDD;
+      line-height: 1.6;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Tejasvi Educates</h1>
+    <p>Chartered Accountant by profession, teacher by passion. Embracing every moment with boundless joy and a thirst for adventure. Join me on a journey of learning and exploration!</p>
+  </div>
+</body>
+</html>
